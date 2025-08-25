376e8661257125461354c3db2792d1e041c32b6f
rapite <andy02lee1@gmail.com>
2025-08-25 16:44:34 -0400

    whitespace

--- include/order.hpp
+++ include/order.hpp
@@ -27,38 +27,38 @@ enum class Side {
 };
 
 class Order {
-    public:
-        Order() = delete; // delete default constructor
+public:
+    Order() = delete; // delete default constructor
 
-        // custom constructor to replace default constructor
-        Order(Side, double, int, OrderType, TimeInForce, Instrument);
- 
-        Order(const Order&) = delete; // delete copy constructor
-        
-        Order& operator=(const Order&) = delete; // delete copy assignment
-        
-        Order(Order&&) = delete; // delete move constructor
-        
-        Order& operator=(Order&&) = delete; // delete move assignment
-       
-        Side side() const { return side_; }
-        int price() const { return price_; }
-        int quantity() const { return quantity_; }
-        int remaining_quantity() const { return remaining_quantity_; }
-        OrderType type() const { return type_; }
-        TimeInForce tif() const { return tif_; }
-        Instrument sym() const {return sym_; }
+    // custom constructor to replace default constructor
+    Order(Side, double, int, OrderType, TimeInForce, Instrument);
 
-        ~Order() = default;
+    Order(const Order&) = delete; // delete copy constructor
+    
+    Order& operator=(const Order&) = delete; // delete copy assignment
+    
+    Order(Order&&) = delete; // delete move constructor
+    
+    Order& operator=(Order&&) = delete; // delete move assignment
+    
+    Side side() const { return side_; }
+    int price() const { return price_; }
+    int quantity() const { return quantity_; }
+    int remaining_quantity() const { return remaining_quantity_; }
+    OrderType type() const { return type_; }
+    TimeInForce tif() const { return tif_; }
+    Instrument sym() const {return sym_; }
 
-    private:
-        Side side_;
-        double price_;
-        int quantity_;
-        int remaining_quantity_;
-        OrderType type_;
-        TimeInForce tif_;
-        Instrument sym_;
+    ~Order() = default;
+
+private:
+    Side side_;
+    double price_;
+    int quantity_;
+    int remaining_quantity_;
+    OrderType type_;
+    TimeInForce tif_;
+    Instrument sym_;
 };
 
 #endif
