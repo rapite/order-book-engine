4b7e9b281eefc39ba70bdfb9536cc1d62e506724
rapite <andy02lee1@gmail.com>
2025-08-26 21:18:42 -0400

    updated API for clarity & orderID

--- include/order.hpp
+++ include/order.hpp
@@ -31,29 +31,30 @@ public:
     Order() = delete; // delete default constructor
 
     // custom constructor to replace default constructor
-    Order(Side, double, int, OrderType, TimeInForce, Instrument);
+    Order(Side, double, int, int, OrderType, TimeInForce, Instrument);
 
     Order(const Order&) = delete; // delete copy constructor
-    
+
     Order& operator=(const Order&) = delete; // delete copy assignment
     
     Order(Order&&) = delete; // delete move constructor
     
     Order& operator=(Order&&) = delete; // delete move assignment
     
-    Side side() const { return side_; }
-    int price() const { return price_; }
-    int quantity() const { return quantity_; }
-    int remaining_quantity() const { return remaining_quantity_; }
-    OrderType type() const { return type_; }
-    TimeInForce tif() const { return tif_; }
-    Instrument sym() const {return sym_; }
+    Side GetSide() const { return side_; }
+    int GetPrice() const { return price_; }
+    int GetQuantity() const { return quantity_; }
+    int GetRemainingQuantity() const { return remaining_quantity_; }
+    OrderType GetType() const { return type_; }
+    TimeInForce GetTIF() const { return tif_; }
+    Instrument GetSym() const {return sym_; }
 
     ~Order() = default;
 
 private:
     Side side_;
     double price_;
+    int orderID_;
     int quantity_;
     int remaining_quantity_;
     OrderType type_;
