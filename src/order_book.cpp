eb2b2f29fa5fbfd2425c13d53a16223de3d85767
rapite <andy02lee1@gmail.com>
2025-08-27 19:07:31 -0400

    decided on add order design

---
+++ src/order_book.cpp
@@ -0,0 +1,17 @@
+#include "order_book.hpp"
+
+void OrderBook::new_order(std::unique_ptr<Order> order) {
+    // Create 
+}
+
+std::optional<Order> OrderBook::best_bid() const {
+    return {};
+}
+
+std::optional<Order> OrderBook::best_ask() const {
+    return {};
+}
+
+OrderBook::~OrderBook() {
+
+}
